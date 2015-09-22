93fa5f7f5219aa787c534f752d86e74e5177768f
Chris Trevino <chris.trevino@atsid.com>
2015-09-22 10:26:50 -0700

    #10 - Add Gulp Archive Task; Add Deploy to Travis

--- gulp/tasks/archive.js
+++ gulp/tasks/archive.js
@@ -1,3 +1,14 @@
-/**
- * Created by darthtrevino on 9/22/15.
- */
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+
+gulp.task('archive', () => {
+  return gulp.src([
+    '{client,config,public,scripts,server}/**/*',
+    'index.js',
+    'package.json',
+    'README.md',
+    'npm-shrinkwrap.json'
+  ])
+    .pipe(zip('badger.zip'))
+    .pipe(gulp.dest('dist'));
+});
