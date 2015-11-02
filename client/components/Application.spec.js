87019cad625650aa4e77c2df5b06c752f09710ea
Chris Trevino <chris.trevino@atsid.com>
2015-11-02 14:42:59 -0800

    #30 - Update Component Import in Test

--- client/components/Application.spec.js
+++ client/components/Application.spec.js
@@ -2,7 +2,7 @@ require('../common.spec/spec.helpers');
 const { expect } = require('chai');
 const React = require('react');
 const ReactTestUtils = require('react-addons-test-utils');
-const Application = require('./application');
+const Application = require('./Application');
 
 describe('Application Component', () => {
   it('should load', () => {
