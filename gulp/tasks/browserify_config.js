1ed79d8cec204ce9ca4584a14a32f0dc78938d48
Chris Trevino <chris.trevino@atsid.com>
2015-11-02 14:12:13 -0800

    #30 - Use Aliasify to Lock Down React Import

--- gulp/tasks/browserify_config.js
+++ gulp/tasks/browserify_config.js
@@ -1,13 +1,13 @@
 const babelify = require('babelify');
 const config = require('../config');
-
+const path = require('path');
 
 module.exports = () => {
   return {
     entries: config.client.entries,
     transform: [
       babelify,
-
+      'aliasify',
       'browserify-shim',
     ],
     debug: false,
