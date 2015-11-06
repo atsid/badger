3850a58690dcbcf65a4af23bd3747bad57913241
Chris Trevino <chris.trevino@atsid.com>
2015-11-06 14:55:13 -0800

    (#42) - Update Vagrant Forwarded Port
    Also, CD'ing into the /app directory on SSH. And installing pip tool 'thefuck'

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ Vagrant.configure("2") do |config|
   config.ssh.forward_agent = true
 
   config.vm.network "private_network", ip: "10.10.10.10"
-  config.vm.network :forwarded_port, guest: 3000, host: 3000
+  config.vm.network :forwarded_port, guest: 9000, host: 9000
   config.vm.network :forwarded_port, guest: 35729, host: 35729
 
   # See NOTE1
--- cookbooks-local/dev-environment/recipes/default.rb
+++ cookbooks-local/dev-environment/recipes/default.rb
@@ -1,7 +1,15 @@
+include_recipe 'python'
+
+python_pip 'thefuck'
+
 execute "Set zsh at the Default Shell" do
   command "chsh -s $(which zsh) vagrant"
 end
 
 execute "Enable Git PreloadIndex Config" do
   command "cd /app && git config core.preloadindex true"
+end
+
+execute "CD to Vagrant Dir on Login" do
+  command "echo 'cd /app' >> /home/vagrant/.zshrc"
 end
