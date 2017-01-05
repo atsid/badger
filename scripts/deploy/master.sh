a69c942910d0a8432d77c470261022670adb1065
Chris Trevino <darthtrevino@gmail.com>
2017-01-05 11:38:25 -0800

    (#45) - Add API stage variable to webpack

--- scripts/deploy/master.sh
+++ scripts/deploy/master.sh
@@ -2,5 +2,6 @@
 export GITHUB_CLIENT_ID=$INTEG_GH_CLIENT_ID
 export CF_DISTRO_ID=$INTEG_CF_DISTRO_ID
 export DEPLOY_BUCKET=$INTEG_DEPLOY_BUCKET
+export API_STAGE="integ"
 export NODE_ENV="production"
 npm-run-all bundle publish
