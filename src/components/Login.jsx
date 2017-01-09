a4815543c8a805cf51e94f8b66871f7eff205368
Chris Trevino <darthtrevino@gmail.com>
2017-01-09 11:17:05 -0800

    (#45) - Logging the URL we are using to fetch the GH OAuth code initially

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 
+const log = require('debug')('app:components:Login');
+
 const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`;
 
 const Login = () => {
   const handleClick = () => {
+    log('Fetching Github Code at url', GITHUB_AUTH_URL);
     window.open(GITHUB_AUTH_URL);
   };
 
